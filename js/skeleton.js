bfe6745587da655ce86d34c91301a6ca4a2cf9f3
woohee Byun <cay107@naver.com>
2022-06-06 17:31:35 +0900

    add [skeleton UI]: skeleton scss,js 추가
    1.scss추가
    2.js 추가

---
+++ js/skeleton.js
@@ -0,0 +1,9 @@
+const loadingItem = document.querySelectorAll('.loading');
+// 로딩 요소, 표시하지 않기
+const hideLoading = () => {
+  loadingItem.forEach(element => {
+    element.classList.add('loading-hidden');
+  });
+}
+// 테스트 코드
+window.onload = setTimeout(hideLoading, 2000);
