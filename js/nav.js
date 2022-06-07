6f6ce0f9c7ae975160f65852064a4f0342ed7578
Sujong Chae <phyzee@naver.com>
2022-06-08 00:34:14 +0900

    feat[deliveryCategory]: featured tab UI, a11y
    1. featured tab UI with JavaScript
    2. used role, aria-labelledby, aria-selected, etc. for a11y
    
    KA1-53

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,39 @@
 const nav = () => {
-  const nav_li = document.querySelectorAll('.nav__li a');
+  const nav_li = document.querySelectorAll(".nav__li a");
+  console.log(nav_li)
 
   const liHandler = (el, e) => {
     e.preventDefault();
-    nav_li.forEach(x => {
-      if (x.classList.contains('selected')) {
-        x.classList.remove('selected');
-        x.removeAttribute('aria-selected');
+    nav_li.forEach((x) => {
+      if (x.classList.contains("selected")) {
+        const openedTab = x.getAttribute("aria-controls");
+        x.classList.remove("selected");
+        x.setAttribute("aria-selected", "false");
+        const openedTabPannel = document.getElementById(openedTab);
+        openedTabPannel.style.display = "none";
+        openedTabPannel.style.paddingTop = "0";
       }
     });
-    el.classList.add('selected');
-    el.setAttribute('aria-selected', 'true');
-  }
 
-  nav_li.forEach(x => x.addEventListener('click', (e) => liHandler(x, e)));
-}
+    const selectedTab = el.getAttribute("aria-controls");
+    el.classList.add("selected");
+    el.setAttribute("aria-selected", "true");
+    const selectedTabPannel = document.getElementById(selectedTab);
+    selectedTabPannel.style.display = "block";
+    selectedTabPannel.style.paddingTop = "100px";
+  };
+
+  const render = () => {
+    nav_li[0].classList.add("selected");
+    nav_li[0].setAttribute("aria-selected", "true");
+    const firstTabtoShow = nav_li[0].getAttribute("aria-controls");
+    const firstSelectedPannel = document.getElementById(firstTabtoShow);
+    firstSelectedPannel.style.display = "block";
+    firstSelectedPannel.style.paddingTop = "100px";
+  };
+
+  render();
+  nav_li.forEach((x) => x.addEventListener("click", (e) => liHandler(x, e)));
+};
 
 export default nav;
