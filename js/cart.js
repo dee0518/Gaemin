337300db10009d7b6fdf905cef08822a366a0333
liketiger <edx2fire@likelion.org>
2022-06-09 23:57:45 +0900

    fix[cart page]: badge reload fix 1. made badge of cart page disappear without reloading when cart__delete btn is pressed 2. removing the ordered items and bill content when cart__delete btn is pressed KA1-40

--- js/cart.js
+++ js/cart.js
@@ -4,9 +4,17 @@ header();
 
 const cart = () => {
   const delete_btn = document.querySelector('.cart__delete');
+  const bill = document.querySelector('.bill');
+  const tung1 = document.querySelector('.cart__order__detail h2');
+  const tung2 = document.querySelector('.cart__order__detail article');
+  const total_count = document.querySelector('.total__count--group');
   delete_btn.addEventListener('click', () => {
-    localStorage.clear();
-    location.reload();
+    localStorage.clear();     
+    // location.reload();
+    bill.classList.add('off');
+    tung1.style.display = 'none';    
+    tung2.style.display = 'none';
+    total_count.style.display = 'none'; 
   });
 }
 
