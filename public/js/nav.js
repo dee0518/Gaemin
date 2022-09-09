12c09addcf1a86d4df5055b6e87ad313651e590b
dee0518 <dehya-518@naver.com>
2022-09-09 15:36:37 +0900

    :recycle: nav.js 1차 코드 리팩토링

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,41 +1,18 @@
 const nav = () => {
-  const nav_li = document.querySelectorAll('.nav__li a');
-  console.log(nav_li);
+  const navList = document.querySelector('.nav__sub__list');
+  const navItems = document.querySelectorAll('.nav__sub__list__item');
 
-  const liHandler = (el, e) => {
+  navList.addEventListener('click', e => {
+    if (!e.target.closest('.nav__sub__list__item')) return;
     e.preventDefault();
-    nav_li.forEach((x) => {
-      if (x.classList.contains('selected')) {
-        const openedTab = x.getAttribute('aria-controls');
-        x.classList.remove('selected');
-        x.setAttribute('aria-selected', 'false');
-        const openedTabPannel = document.getElementById(openedTab);
-        openedTabPannel.style.display = 'none';
-        openedTabPannel.style.paddingTop = '0';
-      }
+
+    navItems.forEach(item => {
+      const isSame = item === e.target.closest('li');
+
+      item.classList.toggle('selected', isSame);
+      item.querySelector('a').setAttribute('aria-selected', isSame);
     });
-
-    const selectedTab = el.getAttribute('aria-controls');
-    el.classList.add('selected');
-    el.setAttribute('aria-selected', 'true');
-    const selectedTabPannel = document.getElementById(selectedTab);
-    selectedTabPannel.style.display = 'block';
-    selectedTabPannel.style.paddingTop = '100px';
-  };
-
-  const render = () => {
-    nav_li[4].classList.add('selected');
-    nav_li[4].setAttribute('aria-selected', 'true');
-    const firstTabtoShow = nav_li[4].getAttribute('aria-controls');
-    const firstSelectedPannel = document.getElementById(firstTabtoShow);
-    firstSelectedPannel.style.display = 'block';
-    firstSelectedPannel.style.paddingTop = '100px';
-  };
-
-  render();
-  nav_li.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'A') return;
-    liHandler(e.target, e);
   });
 };
+
 export default nav;
